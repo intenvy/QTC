5ea6339de258ab67787ae0d0b37aeb7b7374fa42
Parsa Saberi <parsa.xmule@gmail.com>
2020-12-20 15:08:34 +0330

    implement assingment_fetcher
    50%

--- qtc/main/src/scrapwise/scrapwise/spiders/assignment_fetcher.py
+++ qtc/main/src/scrapwise/scrapwise/spiders/assignment_fetcher.py
@@ -1,14 +1,19 @@
-from scrapy import Spider, FormRequest
-
+from scrapy import Spider, FormRequest, Request
 from .quera_crawler import QueraCrawler
 
 
 class AssignmentFetcher(QueraCrawler):
 
-    def __init__(self, assignment_id: int, problem_id: int, username: str, password: str, *args, **kwargs):
+    def __init__(self, assignment_id: int, username: str, password: str, *args, **kwargs):
         super().__init__(username, password, args, kwargs)
         self.assignment_id = assignment_id
-        self.problem_id = problem_id
 
-    def after_login(self, response):
-        pass
+    def after_login(self, response) -> Request:
+        super().after_login(response)
+        return Request(
+            url=f'https://quera.ir/course/assignments/{self.assignment_id}/problems',
+            callback=self.collect_assignment_problems
+        )
+
+    def collect_assignment_problems(self, request):
+        print('Im collecting :(')
